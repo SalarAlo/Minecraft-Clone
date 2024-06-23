aa763e0cb62e878a02e5d1540c6478d9fe56013c
Salar <141232193+SalarAlo@users.noreply.github.com>
2024-06-23 20:18:43 +0200

    MeshData

---
+++ Assets/Scripts/ChunkData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ChunkData 
+{
+    public BlockType[] blocks;
+    public int chunkSize = 16;
+    public int chunkHeight = 100;
+    public World worldRef;
+    public Vector3Int worldPos;
+
+    public bool modifiedByPlayer = false;
+
+
+    public ChunkData(int chunkSize, int chunkHeight, World world, Vector3Int worldPos) {
+        this.chunkSize = chunkSize;
+        this.chunkHeight = chunkHeight;
+        worldRef = world;
+        this.worldPos = worldPos;
+        blocks = new BlockType[chunkSize*chunkHeight*chunkSize];
+    }
+}
---
+++ Assets/Scripts/MeshData.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MeshData
+{
+    public List<Vector3> vertices = new List<Vector3>();
+    public List<int> triangles = new List<int>();
+    public List<Vector2> uv = new List<Vector2>();
+
+    public List<Vector3> colliderVertices = new List<Vector3>();
+    public List<int> colliderTriangles = new List<int>();
+    
+    public MeshData waterMesh;
+    private bool isMainMesh;
+
+    public MeshData(bool isMainMesh){
+        if(isMainMesh)
+            waterMesh = new MeshData(false);
+    }
+
+    public void AddVertex(Vector3 vertex, bool isColliderVertex) {
+        vertices.Add(vertex);
+        if(isColliderVertex) colliderVertices.Add(vertex);
+    }
+
+    public void AddQuadTriangles(bool isColliderQuad) {
+        triangles.Add(vertices.Count - 4);
+        triangles.Add(vertices.Count - 3);
+        triangles.Add(vertices.Count - 2);
+
+        triangles.Add(vertices.Count - 4);
+        triangles.Add(vertices.Count - 2);
+        triangles.Add(vertices.Count - 1);
+
+        if(!isColliderQuad) return;
+
+        colliderTriangles.Add(vertices.Count - 4);
+        colliderTriangles.Add(vertices.Count - 3);
+        colliderTriangles.Add(vertices.Count - 2);
+
+        colliderTriangles.Add(vertices.Count - 4);
+        colliderTriangles.Add(vertices.Count - 2);
+        colliderTriangles.Add(vertices.Count - 1);
+    }
+}
